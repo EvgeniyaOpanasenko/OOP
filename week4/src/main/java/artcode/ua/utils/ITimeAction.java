944915c3823550d8110b333a4bb85552efbef500
Lisa <Lisa>
2017-05-11 15:07:39 +0300

    test action time

---
+++ week4/src/main/java/artcode/ua/utils/ITimeAction.java
@@ -0,0 +1,9 @@
+package artcode.ua.utils;
+
+/**
+ * Created by User on 5/11/2017.
+ */
+public interface ITimeAction {
+
+    public void action();
+}
---
+++ week4/src/main/java/artcode/ua/utils/TimeCounter.java
@@ -0,0 +1,16 @@
+package artcode.ua.utils;
+
+/**
+ * Created by User on 5/11/2017.
+ */
+public class TimeCounter {
+
+    public static String countActionTime(ITimeAction method ){
+
+        long start = System.nanoTime();
+        method.action();
+        long end = System.nanoTime();
+
+        return "nanos" + (end - start);
+    }
+}
