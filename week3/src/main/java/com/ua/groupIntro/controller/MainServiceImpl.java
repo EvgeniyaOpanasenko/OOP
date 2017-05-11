dd0c4f80c17fb867c2edd7b415d7de4ae98d5fcd
Lisa <Lisa>
2017-05-11 14:45:37 +0300

    week4 add

--- week3/src/main/java/com/ua/groupIntro/controller/MainServiceImpl.java
+++ week3/src/main/java/com/ua/groupIntro/controller/MainServiceImpl.java
@@ -57,16 +57,6 @@ public class MainServiceImpl implements MainService {
         return humanList;
     }
 
-  /*  @Override
-    public List<Human> sortWorkerByName() {
-        humanList.sort(new Comparator<Human>() {
-            @Override
-            public int compare(Human o1, Human o2) {
-                return o1.getName().compareTo(o2.getName());
-            }
-        });
-        return humanList;
-    }*/
 
     @Override
     public List<Human> sortWorkerByName() {
