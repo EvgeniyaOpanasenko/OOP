b176b24c2df33f0fc4323802ae41aa93199e973a
Lisa <Lisa>
2017-05-24 22:33:50 +0300

    rename class

--- Sortings/src/main/java/sortings/bubble/SosrtingTests.java
+++ Sortings/src/main/java/sortings/bubble/SosrtingTests.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /**
  * Created by User on 5/24/2017.
  */
-public class Bubble {
+public class SosrtingTests {
     public static void main(String[] args) {
 
         int[] arr = {2, 2, 45, 0, 87, 3, 15};
@@ -16,7 +16,7 @@ public class Bubble {
         Sortings.bubbleSorting(arr);
 
         System.out.println();
-        System.out.println("Bubble sorting");
+        System.out.println("SosrtingTests sorting");
         (Arrays.stream(arr)).forEach(i -> System.out.print("[" + i + "]"));
 
 
